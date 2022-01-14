43bf5c39e9083dadb2664d127813672a70ee47a5
Keith Wagner <244694+kpwags@users.noreply.github.com>
2022-01-14 18:55:00 -0500

    Added User Toggle to Header

--- frontend/src/components/Elements/NavBar/index.tsx
+++ frontend/src/components/Elements/NavBar/index.tsx
@@ -10,17 +10,28 @@ type NavBarProps = {
 
 const NavBar = ({
     selectedItem,
-}: NavBarProps): JSX.Element => (
-    <Header className="nav">
-        <div className="logo">Digital Family Cookbook</div>
-        <Menu theme="dark" mode="horizontal" selectedKeys={[selectedItem]} className="nav-bar-menu">
-            <Menu.Item key="categories">Categories</Menu.Item>
-            <Menu.Item key="meats">Meats</Menu.Item>
-            <Menu.Item key="1" icon={<UserOutlined />} className="user-menu">
-                User
-            </Menu.Item>
-        </Menu>
-    </Header>
-);
+}: NavBarProps): JSX.Element => {
+    const isUserLoggedIn = false;
+
+    return (
+        <Header className="nav">
+            <div className="logo">Digital Family Cookbook</div>
+            <Menu theme="dark" mode="horizontal" selectedKeys={[selectedItem]} className="nav-bar-menu">
+                <Menu.Item key="categories">Categories</Menu.Item>
+                <Menu.Item key="meats">Meats</Menu.Item>
+                {isUserLoggedIn ? (
+                    <Menu.Item key="1" icon={<UserOutlined />} className="user-menu">
+                        User
+                    </Menu.Item>
+                ) : (
+                    <>
+                        <Menu.Item key="register">Register</Menu.Item>
+                        <Menu.Item key="login">Sign In</Menu.Item>
+                    </>
+                )}
+            </Menu>
+        </Header>
+    );
+};
 
 export { NavBar };
