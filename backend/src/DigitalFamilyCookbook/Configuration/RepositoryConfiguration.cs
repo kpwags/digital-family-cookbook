7162c00aff73626bd8291a7f990a84eb4f043311
Keith Wagner <244694+kpwags@users.noreply.github.com>
2022-04-16 15:39:09 -0400

    Configured Meat Repository

--- backend/src/DigitalFamilyCookbook/Configuration/RepositoryConfiguration.cs
+++ backend/src/DigitalFamilyCookbook/Configuration/RepositoryConfiguration.cs
@@ -8,6 +8,9 @@ namespace DigitalFamilyCookbook.Configuration
         public static void AddRepositories(this IServiceCollection services)
         {
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IMeatRepository, MeatRepository>();
+            services.AddScoped<IRecipeCategoryRepository, RecipeCategoryRepository>();
+            services.AddScoped<IRecipeMeatRepository, RecipeMeatRepository>();
             services.AddScoped<ISystemRepository, SystemRepository>();
             services.AddScoped<IUserAccountRepository, UserAccountRepository>();
         }
--- backend/src/DigitalFamilyCookbook/Migrations/20220416192300_CorrectMeatTable.Designer.cs
+++ backend/src/DigitalFamilyCookbook/Migrations/20220416192300_CorrectMeatTable.Designer.cs
@@ -4,6 +4,7 @@ using DigitalFamilyCookbook.Data.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DigitalFamilyCookbook.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220416192300_CorrectMeatTable")]
+    partial class CorrectMeatTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
